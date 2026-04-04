5c2891ddb243897dc325a6c2864e54daa9ac5e5a
iharish17 <241448@kit.ac.in>
2026-04-05 01:08:49 +0530

    Updates

--- backend/data/mockPnrs.js
+++ backend/data/mockPnrs.js
@@ -3,13 +3,13 @@ const mockPnrs = {
     pnr: "4928174021",
     trainNumber: "01211",
     source: "BADNERA JN",
-    destination: "NASIK ROAD",
+    destination: "MANMAD JN",
     passengers: [
       { name: 'Anjali Sharma', age: 28, gender: 'F', coach: 'S4', seat: '12', status: 'CNF' },
       { name: 'Vikram Sharma', age: 30, gender: 'M', coach: 'S4', seat: '13', status: 'CNF' }
     ]
   },
-  "5475698745": {
+   "5475698745": {
     pnr: "5475698745",
     trainNumber: "18102",
     source: "JAT",
@@ -18,50 +18,32 @@ const mockPnrs = {
       { name: 'Harish Kumar', age: 18, gender: 'M', coach: 'B2', seat: '41', status: 'CNF' }
     ]
   },
+  "5475694597": {
+    pnr: "5475694597",
+    trainNumber: "01211",
+    source: "AKOLA JN",
+    destination: "NASIK ROAD",
+    passengers: [
+      { name: 'Harish Kumar', age: 18, gender: 'M', coach: 'B2', seat: '41', status: 'CNF' }
+    ]
+  },
   "1234567890": {
     pnr: "1234567890",
     trainNumber: "01209",
-    source: "NAGPUR",
-    destination: "PUNE JN",
+    source: "BADNERA JN",
+    destination: "MANMAD JN",
     passengers: [
       { name: 'Amit Singh', age: 40, gender: 'M', coach: 'B1', seat: '12', status: 'CNF' }
     ]
   },
+  
   "0987654321": {
     pnr: "0987654321",
-    trainNumber: "01210",
-    source: "PUNE JN",
-    destination: "NAGPUR",
+    trainNumber: "01209",
+    source: "AKOLA JN",
+    destination: "PUNE JN",
     passengers: [
       { name: 'Pooja Verma', age: 25, gender: 'F', coach: 'S2', seat: '54', status: 'CNF' }
-    ]
-  },
-  "1111222233": {
-    pnr: "1111222233",
-    trainNumber: "01211",
-    source: "AKOLA JN",
-    destination: "MANMAD JN",
-    passengers: [
-      { name: 'Karan Johar', age: 50, gender: 'M', coach: 'A2', seat: '11', status: 'CNF' }
-    ]
-  },
-  "9988776655": {
-    pnr: "9988776655",
-    trainNumber: "01212",
-    source: "MANMAD JN",
-    destination: "AKOLA JN",
-    passengers: [
-      { name: 'Raj Kumar', age: 29, gender: 'M', coach: 'D1', seat: '72', status: 'CNF' }
-    ]
-  },
-  "5544332211": {
-    pnr: "5544332211",
-    trainNumber: "01209",
-    source: "BHUSAVAL JN",
-    destination: "MANMAD JN",
-    passengers: [
-      { name: 'Sneha Patel', age: 32, gender: 'F', coach: 'S5', seat: '22', status: 'CNF' },
-      { name: 'Vikas Patel', age: 35, gender: 'M', coach: 'S5', seat: '23', status: 'CNF' }
     ]
   }
 };
